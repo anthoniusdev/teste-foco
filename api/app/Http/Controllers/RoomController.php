7a0cd7ee64ae1f45049d24dbcdeccacc264c2bf7
Anthonius <anthoniusmiguel@gmail.com>
2024-10-30 18:35:06 -0300

    :sparkles: feat: Implement CRUD on Room model

--- api/app/Http/Controllers/RoomController.php
+++ api/app/Http/Controllers/RoomController.php
@@ -12,7 +12,19 @@ class RoomController extends Controller
      */
     public function index()
     {
-        //
+        $rooms = Room::all();
+        if ($rooms->isEmpty()) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'No rooms found',
+                'data' => null
+            ], 404);
+        }
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Rooms retrieved successfully',
+            'data' => $rooms
+        ], 200);
     }
 
     /**
@@ -20,30 +32,90 @@ class RoomController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'Name' => 'required|string',
+            'hotelCode' => 'required|integer',
+        ]);
+
+        $room = Room::create($request->all());
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Room created successfully',
+            'data' => $room
+        ], 201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Room $room)
+    public function show(int $id)
     {
-        //
+        $room = Room::findOrFail($id);
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Room retrieved successfully',
+            'data' => $room
+        ], 200);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Room $room)
+    public function update(Request $request, int $id)
     {
-        //
+        $request->validate([
+            'Name' => 'required|string',
+            'hotelCode' => 'required|integer',
+        ]);
+
+        $room = Room::findOrFail($id);
+
+        $room->update($request->all());
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Room updated successfully',
+            'data' => $room
+        ], 200);
+    }
+
+    public function updatePartial(Request $request, int $id)
+    {
+        $room = Room::findOrFail($id);
+
+        if ($request->has('Name') || $request->has('hotelCode')) {
+            $request->validate([
+                'Name' => 'string',
+                'hotelCode' => 'integer',
+            ]);
+            $room->update($request->all());            
+            return response()->json([
+                'status' => 'success',
+                'message' => 'Room updated successfully',
+                'data' => $room
+            ], 200);
+        }else{
+            return response()->json([
+                'status' => 'error',
+                'message' => 'No data to update',
+                'data' => null
+            ], 400);
+        }
+
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Room $room)
+    public function destroy(int $id)
     {
-        //
+        Room::findOrFail($id)->delete();
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Room deleted successfully',
+            'data' => null
+        ], 200);
     }
 }
--- api/routes/api.php
+++ api/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\RoomController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('/room')->group(function () {
+    Route::get('/', [RoomController::class, 'index']);
+    Route::post('/', [RoomController::class, 'store']);
+    Route::get('/{id}', [RoomController::class, 'show']);
+    Route::put('/{id}', [RoomController::class, 'update']);
+    Route::patch('/{id}', [RoomController::class, 'updatePartial']);
+    Route::delete('/{id}', [RoomController::class, 'destroy']);
 });
